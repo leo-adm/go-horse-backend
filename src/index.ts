78b0eee1ee1db8d92ae9aba694d0c2ca4d889c83
ldalmass <leoaverdalmass@gmail.com>
2025-01-10 21:07:14 -0300

    feat: adds clerk webhook endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
 import {
-  clerkClient,
   clerkMiddleware,
   ExpressRequestWithAuth,
   requireAuth,
@@ -20,6 +19,8 @@ import {
 } from "./types/types";
 import { users } from "./db/schema";
 import { eq, sql } from "drizzle-orm";
+import bodyParser from "body-parser";
+import { Webhook } from "svix";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
@@ -50,8 +51,91 @@ let updatePositionsInterval: NodeJS.Timeout | null = null;
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 app.use(clerkMiddleware());
+
+app.post(
+  "/webhooks",
+  bodyParser.raw({ type: "application/json" }),
+  async (req, res) => {
+    const SIGNING_SECRET = process.env.SIGNING_SECRET
+
+    if (!SIGNING_SECRET) {
+      throw new Error(
+        "Error: Please add SIGNING_SECRET from Clerk Dashboard to .env"
+      )
+    }
+
+    // Create new Svix instance with secret
+    const wh = new Webhook(SIGNING_SECRET)
+
+    // Get headers and body
+    const headers = req.headers
+    const payload = req.body
+
+    // Get Svix headers for verification
+    const svix_id = headers["svix-id"]
+    const svix_timestamp = headers["svix-timestamp"]
+    const svix_signature = headers["svix-signature"]
+
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+      return void res.status(400).json({
+        success: false,
+        message: "Error: Missing svix headers",
+      })
+    }
+
+    let evt: any
+
+    // Attempt to verify the incoming webhook
+    // If successful, the payload will be available from 'evt'
+    // If verification fails, error out and return error code
+    try {
+      evt = wh.verify(payload, {
+        "svix-id": svix_id as string,
+        "svix-timestamp": svix_timestamp as string,
+        "svix-signature": svix_signature as string,
+      })
+    } catch (err: any) {
+      console.log("Error: Could not verify webhook:", err.message)
+      return void res.status(400).json({
+        success: false,
+        message: err.message,
+      })
+    }
+
+    const eventType = evt.type
+    if (eventType === "user.created") {
+      const userId = evt.data.id
+
+      try {
+        await db.insert(users).values({
+          id: userId,
+          balance: "0",
+        })
+      } catch (err: any) {
+        console.log("Error: Could not create user:", err.message)
+        return void res.status(400).json({
+          success: false,
+          message: err.message,
+        })
+      }
+    } else {
+      console.log("Error: Unknown event type:", eventType)
+      return void res.status(400).json({
+        success: false,
+        message: "Unknown event type",
+      })
+    }
+
+    return void res.status(200).json({
+      success: true,
+      message: "Webhook received",
+    })
+  }
+)
+
+app.use(express.json());
 
 // Servidor HTTP
 const server = http.createServer(app);
